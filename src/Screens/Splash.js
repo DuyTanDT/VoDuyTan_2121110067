db4f77d7856b58cda97d1a145251f9a91932a6c0
VoDuyTan <0988209772tan@gmail.com>
2024-01-14 07:41:47 +0700

    25/12

---
+++ src/Screens/Splash.js
@@ -0,0 +1,42 @@
+import { View, Text, Image } from "react-native";
+import React, { useEffect } from "react";
+import { myColors } from "../Utils/MyColor";
+import { StatusBar } from "expo-status-bar";
+import { useNavigation } from "@react-navigation/native";
+
+const Splash = () => {
+    const nav=useNavigation()
+  useEffect(() => {   
+    setTimeout(() => {
+        nav.replace('Home')
+    }, 3000);
+  }, []);
+
+  return (
+    <View
+      style={{
+        backgroundColor:"#000000",
+        flex: 1,
+        justifyContent: "center",
+      }}
+    >
+      <StatusBar style="light" />
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: 10,
+        }}
+      >
+        <Image
+          style={{ tintColor: "white", height: 90, width: 220}}
+          source={require("../assets/logo1.png")}
+        />
+   
+      </View>
+    </View>
+  );
+};
+
+export default Splash;
